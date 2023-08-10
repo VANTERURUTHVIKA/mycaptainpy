4d156fdc212bd0abf7a158f32a3d3cb8d42a7547
VANTERURUTHVIKA <141750881+VANTERURUTHVIKA@users.noreply.github.com>
2023-08-10 11:38:28 +0530

    Created using Colaboratory

---
+++ area of circle.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNxjYN3u8rM7bbdkC2yyXrd",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/VANTERURUTHVIKA/mycaptainpy/blob/main/area%20of%20circle.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "FpF_SLeHDafs"
+      },
+      "outputs": [],
+      "source": [
+        "x=float(input(\"radius of circle is:\"))\n",
+        "print(3.14*x*x)"
+      ]
+    }
+  ]
+}
